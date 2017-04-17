0a471adc85b00a3703c7523c92ff31658769021b
Octavio orlando OJEDA ARONIZ <ocojeda-@e2r13p2.42.fr>
2017-04-17 10:14:39 +0200

    supersampler

---
+++ srcs/supersampler.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   supersampler.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ocojeda- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/17 09:45:23 by ocojeda-          #+#    #+#             */
+/*   Updated: 2017/04/17 09:52:41 by ocojeda-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/rtv1.h"
+
+void	super_sampler(t_env *e)
+{
+	int x;
+	int y;
+	int x1;
+	int y1;
+
+	y = 0;
+	y1 = 0;
+	while(y < H / 2)
+	{
+		x = 0;
+		x1 = 0;
+		while(x < W / 2)
+		{ 
+			((unsigned int *)e->data)[x + y * W / 2] = AVERAGE(
+					AVERAGE(e->img_temp[x1 + y1 * W], e->img_temp[(x1 + 1) + (y1 * W)]),
+					AVERAGE(e->img_temp[x1 + (y1 + 1) * W], e->img_temp[(x1 + 1) + (y1 + 1) * W]));	
+			x1 += 2;
+			x++;
+		}
+		y++;
+		y1 += 2;
+	}
+}
