36f1b8b572d6e7faaa4a4c6f9df66b3eed1b755d
Octavio orlando OJEDA ARONIZ <ocojeda-@e2r13p2.42.fr>
2017-04-26 18:05:21 +0200

    test of coulours in cylindre

--- srcs/cylinder.c
+++ srcs/cylinder.c
@@ -6,7 +6,7 @@
 /*   By: ocojeda- <ocojeda-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/24 16:26:32 by myernaux          #+#    #+#             */
-/*   Updated: 2017/04/26 17:27:49 by ocojeda-         ###   ########.fr       */
+/*   Updated: 2017/04/26 17:50:51 by ocojeda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,14 @@
 
 t_color     *compute_color_cylinder(t_env *e, t_vector poi, t_object cylinder)
 {
-    t_color     *color;
+    t_vector	dist_to_light;
+	double		intensity;
+	t_color		*color;
 
-    color = malloc(200);
-    (void)poi;
-	(void)cylinder;
-	(void)e;
-	color->r = 0;
-	color->g = 0;
-	color->b = 200;
+	color = copy_color(cylinder.color);
+	dist_to_light = vec_ope_min(e->light->origin, poi);
+	intensity = 0.5 * ft_map(get_length(dist_to_light), 2000 * e->light->intensity, 500, 200);
+	color_mult(color, intensity);
 	return (color);
 }
 
@@ -42,7 +41,6 @@ double      intersect_cylinder(t_ray ray, t_object cylinder)
         cylinder.radius * cylinder.radius - 
         2 * (ray.origin.x * cylinder.origin.x
 			+ ray.origin.y * cylinder.origin.y);
-   // c = pow(cylinder.origin.x, 2) + pow(cylinder.origin.z, 2) - 1/*pow(cylinder.radius, 2)*/;
     det = b * b - 4 * a * c;
     t0 = 0;t1 = 0;
     if (det >= 0)
@@ -50,48 +48,10 @@ double      intersect_cylinder(t_ray ray, t_object cylinder)
         det = sqrt(det);
         t0 = (-b - det) / (2 * a);
         t1 = (-b + det) / (2 * a);
-        ft_putchar('\n');
-        ft_putnbr(t0);
-        if (t0 > t1 && t1 > 0)
-            return t1;
+        if (t0 > t1)
+            return (t1);
         else
-         return (t0);
+            return (t0);
     }
-    
-  //  ft_putnbr(t0);
-    //ft_putchar('T');
-   // ft_putnbr(t1);
-   // ft_putchar('\n');
-   /* y0 = cylinder.direction.y + t0 * ray.direction.y;
-    y1 = cylinder.direction.y + t1 * ray.direction.y;
-    f++;
-    ft_putchar('\n');
-    ft_putnbr(f);
-    if(y0 < -1)
-    {
-        if(y1 < -1)
-            return(DIST_MAX);
-         temp = t0 + (t1 -t0) * (y0 + 1) / (y0 - y1);
-         if(temp <= 0)
-            return(DIST_MAX);
-         return (temp);
-       
-    }
-    if(y0 >= -1 && y0 <= 1)
-    {
-        if(t0 <= 0)
-          return (DIST_MAX);
-        return (t0); 
-    }
-    if(y0 > 1)
-    {
-        if(y1 > 1)
-          return (DIST_MAX);
-         temp = t0 + (t1 -t0) * (y0 + 1) / (y0 - y1);
-         if(temp <= 0)
-            return(DIST_MAX);
-         return (temp);
-    }
-    return (DIST_MAX);*/
     return DIST_MAX;
 }
